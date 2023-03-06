8b3e01569d38fd32a63a44878cb0e423f1ecfe6c
Rochet brice <brice.974452@gmail.com>
2023-03-06 12:37:10 +0100

    changement de logo quand changement d'url

--- main/src/components/header/header.jsx
+++ main/src/components/header/header.jsx
@@ -1,16 +1,19 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './style/style.scss'
 
+
 function Header() {
 
+    const [logoIndex, setLogoIndex] = useState(true)
 
     return <header className='header'>
         <nav className='header_nav'>
-            <NavLink to='' className='header_nav_link'>Météo</NavLink>
-            {window.location.pathname === "/carousel" ?
+            <NavLink to='' onClick={() => setLogoIndex(true)} className='header_nav_link'>Météo</NavLink>
+            {window.location.pathname === "/carousel" || logoIndex === false ?
                 <i className="fa-solid fa-image header_nav_logo"></i>
                 : <i className="fa-solid fa-cloud header_nav_logo"></i>}
-            <NavLink to='/carousel' className='header_nav_link'>Carousel</NavLink>
+            <NavLink to='/carousel' onClick={() => setLogoIndex(false)} className='header_nav_link'>Carousel</NavLink>
         </nav>
     </header>
 
